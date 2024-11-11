65be5510402d8dcbbd1e1a92f994df8baa57997b
Sylvie8 <87629790+Sylvie8@users.noreply.github.com>
2024-11-11 06:14:05 -0900

    Update MainLibLobby.lua

--- MainLibLobby.lua
+++ MainLibLobby.lua
@@ -17,7 +17,7 @@ lib = lib or t._FIRELIB
 if not t.EAGLE then
     t.EAGLE = true
     local mw = lib.MakeWindow
-    local fhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Null-Fire/main/Core/Libraries/Fire-Lib/Null-Fire%20Only%20page.lua"))()
+    local fhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sylvie8/Song/refs/heads/main/Null-Fire%20Only%20page.lua"))()
     lib.MakeWindow = function(self, options, fireHubWindow)
         local window = mw(self, options)
         if fireHubWindow then
